31c394c643d02ce2256f42d063e6191ac7d78bf7
DenerBarros <denerbarros.eng@gmail.com>
2022-07-29 17:25:14 -0300

    Criação de vários títulos na aplicação

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,19 @@
 import type { NextPage } from 'next'
 import styles from '../styles/home.module.scss';
+import Head from 'next/head';
+import Link from 'next/head';
 
 
 const Home: NextPage = () => {
   return (
-    <h1
-    className={styles.title}>Ola</h1>
+  <>
+    <Head>
+      <title>Inicio | ig.news</title>
+    </Head>
+    <h1 className={styles.title}>      
+      Ola
+    </h1>
+  </>
   )
 }
 
