8c3419c0781a436c76bd47be6f350896695326a7
DenerBarros <denerbarros.eng@gmail.com>
2022-07-29 17:13:17 -0300

    Criando o app com NextJS e Typescript - instalado o SASS

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import styles from '../styles/home.module.scss';
+
+
+const Home: NextPage = () => {
+  return (
+    <h1
+    className={styles.title}>Ola</h1>
+  )
+}
+
+export default Home
